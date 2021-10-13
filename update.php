4f06c2d6c9583f8199fb8ac5c50b684aea1d6e7b
ayaka <ayaka.seven.11@gmail.com>
2021-10-13 16:45:01 +0900

    change placeholder into initial  value to edit for users

--- update.php
+++ update.php
@@ -38,30 +38,30 @@
             <div class="row mb-4">
                 <div class="col">
                 <label for="task" class="form-label">タスク</label>
-                <input type="text" id ="task" name="task" class="form-control" placeholder="<?php print($task['task_name']); ?>">
+                <input type="text" id ="task" name="task" class="form-control" value="<?php print($task['task_name']); ?>">
                 </div>
 
                 <div class="col">
                 <label for="target_time" class="form-label">目標時間</label>
-                <input type="text" id ="target_time" name="target_time" class="form-control" placeholder="<?php print($task['target_time']); ?>">
+                <input type="text" id ="target_time" name="target_time" class="form-control" value="<?php print($task['target_time']); ?>">
                 </div>
             </div>
             <div class="row mb-4">
                 <div class="col">
                 <label for="created_at" class="form-label">開始日時</label>
-                <input type="text" id ="created_at" name="created_at" class="form-control" placeholder="<?php print($task['created_at']); ?>">
+                <input type="text" id ="created_at" name="created_at" class="form-control" value="<?php print($task['created_at']); ?>">
                 </div>
 
                 <div class="col">
                 <label for="finished_at" class="form-label">終了日時</label>
-                <input type="text" id ="finished_at" name="finished_at" class="form-control" placeholder="<?php print($task['tfinished_at']); ?>">
+                <input type="text" id ="finished_at" name="finished_at" class="form-control" value="<?php print($task['tfinished_at']); ?>">
                 </div>
             </div>
 
             <div class="row mb-4">
                 <div class="col">
                 <label for="duration" class="form-label">かかった時間（分）</label>
-                <input type="text" id ="duration" name="duration" class="form-control" placeholder="<?php print($task['duration']); ?>">
+                <input type="text" id ="duration" name="duration" class="form-control" value="<?php print($task['duration']); ?>">
                 </div>
             </div>
             <button type="submit"  class="btn btn-outline-info">登録</button>
