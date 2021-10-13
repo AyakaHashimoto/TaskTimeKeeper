3092061d7582cb017b5cf0b68b12be9f66e2ccd4
ayaka <ayaka.seven.11@gmail.com>
2021-10-13 14:37:42 +0900

    add input items in the update form

--- update.php
+++ update.php
@@ -35,8 +35,36 @@
         
         <form id="form" action="update_do.php" method="post">
             <input type="hidden" name="id" value="<?php print($id); ?>">
-            <textarea name="task" cols="50" rows ="10"><?php print($task['task_name']); ?></textarea><br>
-            <button type="submit">登録</button>
+            <div class="row mb-4">
+                <div class="col">
+                <label for="task" class="form-label">タスク</label>
+                <input type="text" id ="task" name="task" class="form-control" placeholder="<?php print($task['task_name']); ?>">
+                </div>
+
+                <div class="col">
+                <label for="target_time" class="form-label">目標時間</label>
+                <input type="text" id ="target_time" name="target_time" class="form-control" placeholder="<?php print($task['target_time']); ?>">
+                </div>
+            </div>
+            <div class="row mb-4">
+                <div class="col">
+                <label for="created_at" class="form-label">開始日時</label>
+                <input type="text" id ="created_at" name="created_at" class="form-control" placeholder="<?php print($task['created_at']); ?>">
+                </div>
+
+                <div class="col">
+                <label for="finished_at" class="form-label">終了日時</label>
+                <input type="text" id ="finished_at" name="finished_at" class="form-control" placeholder="<?php print($task['tfinished_at']); ?>">
+                </div>
+            </div>
+
+            <div class="row mb-4">
+                <div class="col">
+                <label for="duration" class="form-label">かかった時間（分）</label>
+                <input type="text" id ="duration" name="duration" class="form-control" placeholder="<?php print($task['duration']); ?>">
+                </div>
+            </div>
+            <button type="submit"  class="btn btn-outline-info">登録</button>
         </form>
 
 
