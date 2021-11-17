14965dd5921a10d1c296632f94ed8053328b03bd
ayaka <ayaka.seven.11@gmail.com>
2021-11-17 17:40:23 +0900

    add thanks page

---
+++ join/thanks.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+ <!-- Bootstrap -->
+    <link href="../css/bootstrap.css" rel="stylesheet">
+    <link href="../css/style.css" rel="stylesheet">
+
+	<title>会員登録</title>
+
+</head>
+<body>
+<section class="vh-100" style="background: linear-gradient(45deg, #b8edf5 0%,#17a2b8 83%);">
+<div class="container py-5 h-100">
+    <div class="row d-flex justify-content-center align-items-center h-100">
+      <div class="col-12 col-md-8 col-lg-6 col-xl-5">
+        <div class="card shadow-2-strong" style="border-radius: 1rem;">
+          <div class="card-body text-center p-5">
+		 	 <h3 class="text-info mb-4">会員登録</h3>
+				<p class="lead text-muted mb-4">会員登録が完了しました。</p>
+				<a class ="btn btn-info mt-4" href="../" role="button">ログインする</a>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+</section>
+</body>
+</html>
+
+ <!-- style reference https://mdbootstrap.com/docs/standard/extended/login/  -->
