dcffebbbfbe5f87b998e044eb52fffd6c526f5b9
ayaka <ayaka.seven.11@gmail.com>
2021-12-03 14:38:58 +0900

    modified page design

--- login/logout.php
+++ login/logout.php
@@ -22,9 +22,31 @@ if (isset($_COOKIE["PHPSESSID"])) {
 session_destroy();
 
 setcookie('email', '', time()-3600);
- 
-echo "<p>ログアウトしました。</p>";
- 
-echo "<a href='../index.php'>はじめのページへ</a>";
 
 ?>
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- Bootstrap -->
+    <link href="../css/bootstrap.css" rel="stylesheet">
+    <link href="../css/style.css" rel="stylesheet">
+
+    <title>Task Time Keeper</title>
+</head>
+<body>
+<main>
+        <h2 class="text-center text-info my-4">TASK TIME KEEPER</h2>
+
+        <div class="card">
+            <div class="card-body">
+                <p class="lead mb-4">ログアウトしました。</p>
+                <a class="btn btn-outline-info" href="../index.php" role="button">最初のページへ戻る</a>
+            </div>
+        </div>
+        
+    </main>
+</body>
