ee0fdf706c84e6faf73e8e7204fded521ca745f9
ayaka <ayaka.seven.11@gmail.com>
2021-12-10 18:15:15 +0900

    fixed link to register

--- index.php
+++ index.php
@@ -102,7 +102,7 @@ if(!empty($_POST)){
                 <br>
                 <div class="mb-3">
                     <button type="submit" class="btn btn-outline-primary">ログイン</button>
-                    | <a href="/tasktimekeeper/join/index.php?action=register"> 新規登録</a> |
+                    | <a href="/tasktimekeeper/mail/registration.php"> 新規登録</a> |
                     <a class="btn btn-outline-primary my-2" href="/tasktimekeeper/login/twitterLogin.php">
                     Twitterでログイン</a>
                 </div>
