f9924522f17e8902d52aab9552c9a72d65d93fba
ayaka <ayaka.seven.11@gmail.com>
2022-01-11 11:40:56 +0900

    My first commit

--- dbconnect.php
+++ dbconnect.php
@@ -2,7 +2,7 @@
 <?php
 date_default_timezone_set('Asia/Tokyo');
 try{
-    $db= new PDO('mysql:dbname=tk-db;hosts=127.0.0.1;charset=utf8','root','root');
+    $db= new PDO('mysql:dbname=tk-db;hosts=127.0.0.1;charset=utf8','ttkUser','ttkUser@app');
     //echo 'success';
 }catch(PDOException $e){
     echo 'DB connection error: '.$e->getMessage();
