d95f02eb5988652378e96c79ecf9dd94913d537a
ayaka <ayaka.seven.11@gmail.com>
2022-01-12 13:30:19 +0900

    Modified DB connection info

--- dbconnect.php
+++ dbconnect.php
@@ -2,7 +2,7 @@
 <?php
 date_default_timezone_set('Asia/Tokyo');
 try{
-    $db= new PDO('mysql:dbname=tk-db;hosts=127.0.0.1;charset=utf8','ttkUser','ttkUser@app');
+    $db= new PDO('mysql:dbname=tk-db;hosts=us-cdbr-east-05.cleardb.net;charset=utf8','bbefb6958d3ab1','185a1d9d');
     //echo 'success';
 }catch(PDOException $e){
     echo 'DB connection error: '.$e->getMessage();
