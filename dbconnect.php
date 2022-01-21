1351f528f542807f1de9dbd5d4865b5a6761022a
ayaka <ayaka.seven.11@gmail.com>
2022-01-21 11:43:45 +0900

    deleted dotenv

--- dbconnect.php
+++ dbconnect.php
@@ -3,9 +3,6 @@
 // 自動で読み込み
 require ('vendor/autoload.php');
 
-// .envを使用する
-Dotenv\Dotenv::createImmutable(__DIR__)->load();
-
 date_default_timezone_set('Asia/Tokyo');
 $driver_options = [PDO::MYSQL_ATTR_INIT_COMMAND => "SET time_zone='+09:00'"];
 try{
