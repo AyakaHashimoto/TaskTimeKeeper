5bb2e505f95c524228fb9cfc7caa39fcc98c2b3a
ayaka <ayaka.seven.11@gmail.com>
2022-01-21 12:20:21 +0900

    modified config vals

--- dbconnect.php
+++ dbconnect.php
@@ -2,11 +2,14 @@
 <?php
 // 自動で読み込み
 require ('vendor/autoload.php');
-
 date_default_timezone_set('Asia/Tokyo');
+
+$dsn = "mysql:host=".getenv('DB_HOST').";dbname=".getenv('DB_NAME').";charset=utf8";
+$user = getenv('DB_USER');
+$password = getenv('DB_PASS');
 $driver_options = [PDO::MYSQL_ATTR_INIT_COMMAND => "SET time_zone='+09:00'"];
 try{
-    $db= new PDO(getenv('DB_CONNECT'),$driver_options);
+    $db= new PDO($dsn,$user,$password,$driver_options);
     //echo 'success';
 }catch(PDOException $e){
     echo 'DB connection error: '.$e->getMessage();
